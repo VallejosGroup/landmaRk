6ce57d1ab0173efe1a08cdbf726d5cd7a02121e6
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-06-24 17:32:05 +0100

    Format using air

--- R/landmarking_longitudinal.R
+++ R/landmarking_longitudinal.R
@@ -61,11 +61,10 @@ setMethod(
     cl <- init_cl(cores)
     on.exit(parallel::stopCluster(cl), add = TRUE)
 
-
     if (fda::CRAN() & Sys.info()["sysname"] == "Windows") {
-      `%doparallel%`  <- foreach::`%do%`
+      `%doparallel%` <- foreach::`%do%`
     } else {
-      `%doparallel%`  <- foreach::`%dopar%`
+      `%doparallel%` <- foreach::`%dopar%`
     }
 
     x@longitudinal_fits <- foreach::foreach(landmark = landmarks) %doparallel%
