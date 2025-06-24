b138d8095e761e8ca79d55a579dccc46ad98837b
Víctor Velasco-Pardo <vvelasco@ed.ac.uk>
2025-06-24 14:17:14 +0100

    Visualising longitudinal trajectory + survival curve

--- R/landmarking_performance.R
+++ R/landmarking_performance.R
@@ -112,3 +112,117 @@ setMethod(
     return(scores)
   }
 )
+
+
+performance_metrics_cv <- function(x, landmarks, windows, K = 10, c_index = TRUE, brier = TRUE) {
+  browser()
+  error_str <- NULL
+  if (!inherits(x, "Landmarking")) {
+    error_str <- c(error_str, "@x must be an object of class Landmarking")
+  }
+  if (is(landmarks)[1] != "numeric") {
+    error_str <- c(error_str, "@landmarks must be a vector of numeric values")
+  }
+  if (is(windows)[1] != "numeric") {
+    error_str <- c(error_str, "@windows must be a vector of numeric values")
+  }
+  if (is(c_index)[1] != "logical") {
+    error_str <- c(error_str, "@c_index must be a logical")
+  }
+  if (is(brier)[1] != "logical") {
+    error_str <- c(error_str, "@brier must be a logical")
+  }
+  if (length(error_str) > 0) {
+    stop(paste(error_str, collapse = ". "))
+  }
+
+  x@data_static$cv_fold <- sample(K, size = nrow(x@data_static), replace = TRUE)
+
+  num_longitudinal_fits <- length(x@longitudinal_fits)
+  for (name in names(x@survival_fits)) {
+    # Iterate over every combination of landmark+window
+    for (k in 1:K) {
+      # Iterate over crossvalidation folds
+
+    }
+    if (brier == TRUE) {
+      brier_list[[paste0(landmark, "-", window)]] <-
+        BinaryBrierScore(
+          predictions = predictions,
+          time = dataset$event_time,
+          status = dataset$event_status,
+          tau = horizon,
+          cause = 1
+        )
+    }
+    if (c_index == TRUE) {
+      cindex_list[[paste0(landmark, "-", window)]] <-
+        CIndexCRisks(
+          predictions = predictions,
+          time = dataset$event_time,
+          status = dataset$event_status,
+          tau = horizon,
+          cause = 1,
+          method = "survival",
+          cens.code = 0
+        )
+    }
+  }
+
+
+  scores <- expand.grid(landmark = landmarks, window = windows)
+  brier_list <- list()
+  cindex_list <- list()
+  for (landmark in landmarks) {
+    for (window in windows) {
+      horizon <- landmark + window
+      at_risk_individuals <- x@risk_sets[[as.character(landmark)]]
+
+      # Construct dataset for survival analysis (censor events past horizon time)
+      dataset <- x@data_static[at_risk_individuals, ] |>
+        mutate(
+          event_status = ifelse(
+            get(x@event_time) > horizon,
+            0,
+            get(x@event_indicator)
+          ),
+          event_time = ifelse(
+            get(x@event_time) > horizon,
+            horizon,
+            get(x@event_time)
+          )
+        )
+
+      predictions <- x@survival_predictions[[paste0(landmark, "-", window)]]
+      if (brier == TRUE) {
+        brier_list[[paste0(landmark, "-", window)]] <-
+          BinaryBrierScore(
+            predictions = predictions,
+            time = dataset$event_time,
+            status = dataset$event_status,
+            tau = horizon,
+            cause = 1
+          )
+      }
+      if (c_index == TRUE) {
+        cindex_list[[paste0(landmark, "-", window)]] <-
+          CIndexCRisks(
+            predictions = predictions,
+            time = dataset$event_time,
+            status = dataset$event_status,
+            tau = horizon,
+            cause = 1,
+            method = "survival",
+            cens.code = 0
+          )
+      }
+    }
+  }
+  if (c_index == TRUE) {
+    scores <- cbind(scores, cindex = unlist(cindex_list))
+  }
+  if (brier == TRUE) {
+    scores <- cbind(scores, brier = unlist(brier_list))
+  }
+  return(scores)
+}
--- R/landmarking_visualisation.R
+++ R/landmarking_visualisation.R
@@ -53,3 +53,88 @@ setMethod(
     }
   }
 )
+
+plot_landmarking <- function(x, id, dynamic_covariate, landmark, window, ylim) {
+
+  newdata <- x@data_dynamic[[dynamic_covariate]] |>
+    filter(get(x@ids) == id) |>
+    filter(get(x@times) <= landmark)
+
+  landmark_prediction <-
+    x@longitudinal_predictions[[as.character(landmark)]][[dynamic_covariate]][landmark]
+
+  newdata$colour <- 1
+  newdata <- rbind(newdata, c(id, landmark, landmark_prediction, 2))
+
+  horizon <- landmark + window
+  layout(
+    mat = matrix(c(1,2), nrow = 1, ncol = 2),
+    heights = c(1),
+    widths = c(landmark/horizon, window/horizon)
+  )
+  # par(mfrow = c(1, 2))
+  plot(
+    newdata$time,
+    newdata$value,
+    col = newdata$colour,
+    xlab = "Time",
+    ylab = dynamic_covariate,
+    pch = 16,
+    ylim = ylim,
+    xaxt = "n"
+  )
+  axis(1, at = round(seq(0, landmark, length.out = 5), 2))
+
+  name <- paste0(landmark, "-", window)
+  plot(
+    survfit(x@survival_fits[[name]], data = x@data_static |> filter(get(x@ids) == id)),
+    xaxt = "n",
+    xlab = "Time",
+    ylab = "Survival prob"
+  )
+  axis(1, at = seq(0, window, length.out = 4), labels = round(seq(landmark, landmark+window, length.out = 4), 2))
+  #par(mfrow = c(1, 1))
+}
+
+plot_landmarking2 <- function(x, id, dynamic_covariate, landmark, window) {
+  browser()
+  cat("Hola")
+
+  newdata <- x@data_dynamic[[dynamic_covariate]] |>
+    filter(get(x@ids) == id) |>
+    filter(get(x@times) <= landmark)
+
+  landmark_prediction <-
+    x@longitudinal_predictions[[as.character(landmark)]][[dynamic_covariate]][landmark]
+
+  newdata$colour <- 1
+  newdata <- rbind(newdata, c(id, landmark, landmark_prediction, 2))
+
+  newdata |>
+    ggplot(aes(x = get(x@times), y = get(x@measurements)), ) +
+      geom_point(colour = newdata$colour)
+
+
+  survminer::ggsurvplot(
+    x@survival_fits[[name]],
+    data = newdaa
+  )
+  name <- paste0(landmark, "-", window)
+  survminer::ggadjustedcurves(
+    survfit(x@survival_fits[[name]]),
+    data = x@data_static |> filter(get(x@ids) == id)
+  ) +
+    ggplot2::ggtitle(paste(
+      "Landmark = ",
+      landmark,
+      "Prediction window = ",
+      window
+    )) +
+    ggplot2::scale_x_continuous(
+      breaks = seq(from = 0, to = window, length.out = 4),
+      labels = landmark + seq(from = 0, to = window, length.out = 4)
+    )
+
+
+  ggplot()
+}
