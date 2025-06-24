925ad5df95e667fab9a08044507f5ce3cb8b3a81
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-06-24 14:42:33 +0100

    Fix spelling

--- tests/testthat/test-lcmm_conv.R
+++ tests/testthat/test-lcmm_conv.R
@@ -14,12 +14,12 @@ test_that("lcmm convergence messages are raised", {
   example <- list(conv = 3)
   expect_message(
     check_lcmm_convergence(example),
-    "Convergence criteria satisfied witha partial Hessian matrix."
+    "Convergence criteria satisfied with a partial Hessian matrix."
   )
 
   example <- list(conv = 4)
   expect_error(
     check_lcmm_convergence(example),
-    "Problem occured during optimisation of the LCMM model."
+    "Problem occurred during optimisation of the LCMM model."
   )
 })
