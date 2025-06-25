635f5131c671170bd42b35997173eab7d800e1c9
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-06-25 13:49:42 +0100

    Revert "Update landmarking_longitudinal_aux.R"
    This reverts commit ee279c970a39348e52a796ca5506591b9520b380.

--- R/landmarking_longitudinal_aux.R
+++ R/landmarking_longitudinal_aux.R
@@ -43,6 +43,7 @@ check_method_long_predict <- function(method) {
   method
 }
 
+
 check_riskset <- function(x, landmark) {
   # Check that relevant risk set is available
   if (!(landmark %in% x@landmarks)) {
@@ -98,7 +99,11 @@ init_cl <- function(cores) {
   if (Sys.info()["sysname"] == "Windows") {
     # Use PSOCK on Windows
     cl <- parallel::makeCluster(cores, type = "PSOCK")
-    doParallel::registerDoParallel(cl)
+    parallel::clusterEvalQ(cl, {
+      check_riskset <- landmaRk:::check_riskset
+      check_dynamic_covariate <- landmaRk:::check_dynamic_covariate
+      construct_data <- landmaRk:::construct_data
+    })
 
     doSNOW::registerDoSNOW(cl)
   } else {
