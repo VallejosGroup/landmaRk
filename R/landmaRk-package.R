6ed77dd5cbb791e8e8906771c228982df6f446cc
Víctor Velasco-Pardo <vvelasco@ed.ac.uk>
2025-07-04 18:10:16 +0100

    Summary method for class LandmarkAnalysis (#84)
    * summary method
    
    * Closes #74
    
    * Remove warning
    
    * Format code
    
    * Remove note
    
    * Generic for the show method
    
    * Rename argument
    
    * Add helper function for multiple errors
    
    * Format using Air
    
    * Reduce line length in test
    
    ---------
    
    Co-authored-by: Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>

--- R/landmaRk-package.R
+++ R/landmaRk-package.R
@@ -1,10 +1,11 @@
 #' landmaRk
 ## usethis namespace: start
+#' @import dplyr
+#' @importFrom foreach `%dopar%`
 #' @importFrom methods new is
 #' @importFrom stats as.formula predict model.matrix
+#' @importFrom utils capture.output
 #' @importFrom utils head
-#' @importFrom foreach `%dopar%`
-#' @import dplyr
 ## usethis namespace: end
 #' @docType package
 #' @name landmaRk
