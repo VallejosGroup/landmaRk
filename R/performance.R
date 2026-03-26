a6044c77636235dfdf03607c6c496b41b323de93
Víctor Velasco-Pardo <vvelasco@ed.ac.uk>
2026-03-26 16:37:12 +0000

    Closes #166

--- R/performance.R
+++ R/performance.R
@@ -116,6 +116,22 @@ setMethod(
           horizon
         )]]
       }
+      if (train) {
+        sf   <- x@survival_predictions[[paste0(landmark, "-", horizon)]]
+      } else {
+        sf   <- x@survival_predictions_test[[paste0(landmark, "-", horizon)]]
+      }
+      if (length(h_times) == 0) {
+        sfit <- summary(sf, times = horizon - landmark)
+        n_ind    <- nrow(dataset)
+        n_t      <- 1
+        pred_matrix <- 1- matrix(sfit$surv, nrow = n_ind, ncol = n_t, byrow = TRUE)
+      } else {
+        sfit <- summary(sf, times = h_times)
+        n_ind    <- nrow(dataset)
+        n_t      <- length(h_times)
+        pred_matrix <- 1- matrix(sfit$surv, nrow = n_ind, ncol = n_t, byrow = TRUE)
+      }
       if (sum(dataset$event_status) == 0) {
         stop("No events in the evaluation set")
       }
@@ -125,7 +141,7 @@ setMethod(
           brier_list[[paste0(landmark, "-", horizon)]] <- riskRegression::Score(
             # object = list(x@survival_predictions_test[[paste0(landmark, "-", horizon)]]),
             object = list(x@survival_fits[[paste0(landmark, "-", horizon)]]),
-            formula = x@survival_fits[[paste0(landmark, "-", horizon)]]$formula,
+            formula = Surv(event_time, event_status) ~ 1,
             data = dataset,
             cause = 1,
             times = horizon - landmark,
@@ -135,18 +151,19 @@ setMethod(
             filter(model != "Null model") |>
             pull(Brier)
         } else {
-          brier_list[[paste0(landmark, "-", horizon)]] <- riskRegression::Score(
+          brier_list[[paste0(landmark, "-", horizon)]] <- sapply(seq_along(h_times), function(j) {riskRegression::Score(
             # object = list(x@survival_predictions_test[[paste0(landmark, "-", horizon)]]),
-            object = list(x@survival_fits[[paste0(landmark, "-", horizon)]]),
-            formula = x@survival_fits[[paste0(landmark, "-", horizon)]]$formula,
+            object = list("model" = pred_matrix[, j, drop = FALSE]),
+            formula = Surv(event_time, event_status) ~ 1,
             data = dataset,
             cause = 1,
-            times = h_times,
+            times = h_times[j],
             cens.method = "ipcw",
             cens.model = "km"
           )$Brier$score |>
             filter(model != "Null model") |>
             pull(Brier)
+          })
 
           names(brier_list[[paste0(landmark, "-", horizon)]]) <- paste0(
             "Brier(",
@@ -181,21 +198,21 @@ setMethod(
             timeROC::timeROC(
               T = dataset[, "event_time"],
               delta = dataset[, "event_status"],
-              marker = predictions,
+              marker = horizon - landmark,
               cause = 1,
               times = horizon - landmark
             )$AUC[2]
           )
         } else {
-          auct_list[[paste0(landmark, "-", horizon)]] <- unname(
+          auct_list[[paste0(landmark, "-", horizon)]] <- sapply(seq_along(h_times), function(j) {unname(
             timeROC::timeROC(
               T = dataset[, "event_time"],
               delta = dataset[, "event_status"],
-              marker = predictions,
+              marker = pred_matrix[, j],
               cause = 1,
-              times = h_times
-            )$AUC
-          )
+              times = h_times[j]
+            )$AUC[paste0("t=",h_times[j])]
+          )})
           names(auct_list[[paste0(landmark, "-", horizon)]]) <- paste0(
             "AUC(",
             h_times,
